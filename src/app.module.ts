67af961478999b6add69a8c98c13b5813cc71ad2
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-06 20:18:26 -0300

    Add EmailModule and EmailService to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { EmailService } from './email/email.service';
+import { EmailModule } from './email/email.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [EmailModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EmailService],
 })
 export class AppModule {}
