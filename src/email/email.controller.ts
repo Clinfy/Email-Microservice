2f4dac0fcc5cdb4a886b2d2c271835f535e26e87
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-06 20:46:46 -0300

    Add API key guard and permissions to email sending endpoint; update DTO import paths

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,11 +1,16 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Post, UseGuards} from '@nestjs/common';
 import {EmailService} from "./email.service";
-import {SendEmailDTO} from "../interfaces/DTO/email.dto";
+import {SendEmailDTO} from "../DTO/email.dto";
+import {ApiKeyGuard} from "../middlewares/api-key.middleware";
+import {Permissions} from "../middlewares/decorators/permissions.decorator";
+
 
 @Controller('email')
 export class EmailController {
     constructor(private readonly emailService: EmailService) {}
 
+    @UseGuards(ApiKeyGuard)
+    @Permissions(['EMAIL_SEND'])
     @Post('send')
     sendMail(@Body() dto: SendEmailDTO): Promise<{message: string}> {
         return this.emailService.sendEmail(dto);
