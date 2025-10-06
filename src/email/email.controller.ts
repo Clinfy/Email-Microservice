a008fd2ff328a13f094fd3734d3662cbac90a27a
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-06 20:18:52 -0300

    Add Email module, controller, and service with basic email sending functionality

---
+++ src/email/email.controller.ts
@@ -0,0 +1,13 @@
+import {Body, Controller, Post} from '@nestjs/common';
+import {EmailService} from "./email.service";
+import {SendEmailDTO} from "../DTO/email.dto";
+
+@Controller('email')
+export class EmailController {
+    constructor(private readonly emailService: EmailService) {}
+
+    @Post('send')
+    sendMail(@Body() dto: SendEmailDTO): Promise<{message: string}> {
+        return this.emailService.sendEmail(dto);
+    }
+}
---
+++ src/email/email.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@nestjs/common';
+import {createTransport} from "nodemailer";
+import {ConfigService} from "@nestjs/config";
+import {SendEmailDTO} from "../DTO/email.dto";
+
+@Injectable()
+export class EmailService {
+    constructor(private readonly configService: ConfigService) {}
+
+    async sendEmail(dto: SendEmailDTO): Promise<{message: string}> {
+        const transport = this.emailTransport();
+        try {
+            await transport.sendMail({
+                from: this.configService.get<string>('EMAIL_USER'),
+                to: dto.recipient,
+                subject: dto.subject,
+                html: dto.html,
+                text: dto.text,
+            })
+            return {message: 'Email sent successfully'};
+        }catch (e) {
+            throw new Error(e);
+        }
+    }
+
+    private emailTransport(){
+        return createTransport({
+            host: this.configService.get<string>('EMAIL_HOST'),
+            port: this.configService.get<number>('EMAIL_PORT'),
+            secure: false,
+            auth: {
+                user: this.configService.get<string>('EMAIL_USER'),
+                pass: this.configService.get<string>('EMAIL_PASS'),
+            }
+        });
+    }
+}
