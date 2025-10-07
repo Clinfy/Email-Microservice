57b1652e9d984ef5992f83218188c11cbade7275
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-07 13:38:27 -0300

    Add Swagger documentation setup for Clinify Users Microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {writeFileSync} from "node:fs";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    //Swagger
+    const config = new DocumentBuilder()
+        .setTitle('Clinify Users Microservice')
+        .setDescription('Docs')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
+    SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
