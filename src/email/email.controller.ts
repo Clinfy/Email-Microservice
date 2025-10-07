8684fd73c59336eeb203a405e4e4161db5a537f6
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-07 13:39:04 -0300

    Add Swagger decorators for email sending endpoint documentation

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -3,6 +3,7 @@ import {EmailService} from "./email.service";
 import {SendEmailDTO} from "../DTO/email.dto";
 import {ApiKeyGuard} from "../middlewares/api-key.middleware";
 import {Permissions} from "../middlewares/decorators/permissions.decorator";
+import {ApiBasicAuth, ApiBody, ApiCreatedResponse, ApiOperation} from "@nestjs/swagger";
 
 
 @Controller('email')
@@ -11,6 +12,10 @@ export class EmailController {
 
     @UseGuards(ApiKeyGuard)
     @Permissions(['EMAIL_SEND'])
+    @ApiBasicAuth()
+    @ApiOperation({summary: "Send's an email using nodemailer"})
+    @ApiBody({type: SendEmailDTO})
+    @ApiCreatedResponse({schema: {type: 'object', properties: { message: { type: 'string' }}}})
     @Post('send')
     sendMail(@Body() dto: SendEmailDTO): Promise<{message: string}> {
         return this.emailService.sendEmail(dto);
