6dd7e7d329dae84abc317ee8b41e402ea5ae7169
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-07 20:05:37 -0300

    Remove unused EmailService import from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EmailService } from './email/email.service';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
 @Module({
