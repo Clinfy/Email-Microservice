85b981c47cd32625f173506ddfd9ff78676c62c7
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-12-04 13:13:33 -0300

    Refactor RabbitMQ integration by moving configuration to `main.ts` and simplifying AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,30 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    ClientsModule.registerAsync([
-      {
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        name: 'EMAIL_SERVICE',
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.RMQ,
-          options: {
-            urls: [configService.get<string>('RABBITMQ_URL') as string],
-            queue: 'email_queue',
-            queueOptions: {
-              durable: true,
-            },
-          },
-        }),
-      },
-    ]),
-    EmailModule,
-  ],
+  imports: [ ConfigModule.forRoot(), EmailModule ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { writeFileSync } from 'node:fs';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +20,22 @@ async function bootstrap() {
   writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
   SwaggerModule.setup('docs', app, document);
 
+  //RabbitMQ
+  const configService = app.get(ConfigService);
+  const rabbitMqUrl = configService.get<string>('RABBITMQ_URL') as string;
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [rabbitMqUrl],
+      queue: 'email_queue',
+      wildcards: true,
+      queueOptions: { durable: true },
+      noAck: false
+    }
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
