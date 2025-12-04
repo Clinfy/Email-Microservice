c6877ec9d4d97771d770b4abaea5399cf59f33c7
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-12-04 12:06:19 -0300

    Refactor code style in `main.ts` to improve readability and align with formatting conventions

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {writeFileSync} from "node:fs";
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { writeFileSync } from 'node:fs';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    //Swagger
-    const config = new DocumentBuilder()
-        .setTitle('Clinfy Email Sender Microservice')
-        .setDescription('Docs')
-        .setVersion('1.0')
-        .addBearerAuth()
-        .build();
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Clinfy Email Sender Microservice')
+    .setDescription('Docs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
-    SwaggerModule.setup('docs', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
