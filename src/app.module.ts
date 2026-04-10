e130d180e08000330a153271a9774a77687c3810
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-10 20:09:09 -0300

    Integrate Winston logging module and configure daily rotate file transports for enhanced logging; update providers with `AllExceptionsFilter`.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,45 @@ import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './config/env-validation';
+import { WinstonModule } from 'nest-winston';
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate }),
-    EmailModule
+
+    //Winston Logger Module
+    WinstonModule.forRoot({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.errors({ stack: true }),
+        winston.format.json(),
+      ),
+      transports: [
+        //new winston.transports.Console(),
+        new winston.transports.DailyRotateFile({
+          filename: 'logs/error-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          level: 'error',
+          maxSize: '5m',
+          maxFiles: '14d',
+        }),
+        new winston.transports.DailyRotateFile({
+          filename: 'logs/combined-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          maxSize: '10m',
+          maxFiles: '30d',
+        }),
+      ],
+    }),
+
+    EmailModule,
   ],
 
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AllExceptionsFilter],
 })
 export class AppModule {}
